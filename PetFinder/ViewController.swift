2d52b0abafea94b445b849a8e9ed3c9a8fef5b6c
Ekaterina <anekig@icloud.com>
2025-04-17 15:47:49 +0100

    init the project and remove the main storyboard and its reference

---
+++ PetFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetFinder
+//
+//  Created by Kotya on 17/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
